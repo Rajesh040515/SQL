e5610f872a3b3ed925a2297e1efd3d68bf253ff9
Rajesh <rajesh.facile@outlook.com>
2021-03-17 03:57:13 -0400

    in oprator

--- insert query.sql
+++ insert query.sql
@@ -5,6 +5,5 @@ USE [Test]
 
 
 
- SELECT * FROM  customer WHERE salary < 40000
-  SELECT * FROM  customer WHERE Address = 'ahmedabad' 
+ SELECT * FROM  customer WHERE Address IN('rajkot','ahmedabad') 
 
