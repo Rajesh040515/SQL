56dd781954a759af1ff2678eca7b58dd288191d7
Rajesh <rajesh.facile@outlook.com>
2021-03-18 05:03:34 -0400

    copy table

--- companyquery.sql
+++ companyquery.sql
@@ -1,3 +1,2 @@
-INSERT INTO [Test].[dbo].[customer] (Name, Age, Address, Salary, order_id)
-VALUES ('kuldip',13,'surat',45000,20);
 
+select * into [Test].[dbo].[TotalOrders] from [Test].[dbo].[Orders]
