0aee43e305c8092439fd2f2617740cf97658e544
Rajesh <rajesh.facile@outlook.com>
2021-03-18 06:49:33 -0400

    group by clause

--- companyquery.sql
+++ companyquery.sql
@@ -6,14 +6,11 @@
 --select Name from [Test].[dbo].[customer]
 --where Salary >15000
 
-select MAX(Salary) as Highest,
-Min(Salary) as lowest,
-avg(Salary) as average,
-sum(Salary) as addsalary,
-count(Salary) as counttotal
-from [Test].[dbo].[customer]
-where age>25
+select 
+sum(Salary) as salary
 
+from [Test].[dbo].[customer]	
+group by Name 
 
 
 --select Salary from [Test].[dbo].[customer]
