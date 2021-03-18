ac95403fae3dc321841da727d015dbee14271f0e
Rajesh <rajesh.facile@outlook.com>
2021-03-18 02:23:29 -0400

    right join

--- companyquery.sql
+++ companyquery.sql
@@ -3,7 +3,10 @@ GO
 
 
 
-select * from [Test].[dbo].[customer] e
-join  [Test].[dbo].[customer] n
-on e.Name=n.Name
+select c.Name,
+o.order_id
 
+
+from   [Test].[dbo].[customer] c
+ right join  [Test].[dbo]. [Orders] o
+on c.order_id = o.order_id
